ad3bb472adbd25f68af88baa95d2325b908fc424
zuo999 <2696134133@qq.com>
2019-10-29 17:02:24 +0800

    ...

--- .vscode/10.29.c
+++ .vscode/10.29.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main(void)
 {
-    printf("haha.\n")
+    int a,b,c,d,sum;
+    float aver;
+    printf("Enter a,b,c,d:\n");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+
+
+    sum=a+b+c+d;
+    aver=sum/4.0f;
+    printf("Sum is %d,average is %.1f\n",sum,aver);
     return 0;
 }
